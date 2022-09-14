9752424e3f24141f6e7e1a76ce10f82213d3a244
DucAnh <ducanh.le1908@gmail.com>
2022-09-14 16:58:46 +0700

    add project yoong time

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TopBar from '../../components/TopBar/TopBar';
+
+Home.propTypes = {
+    
+};
+
+function Home(props) {
+    return (
+        <div>
+            <TopBar />
+        </div>
+    );
+}
+
+export default Home;
